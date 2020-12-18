7beb7d836decb57fecfcf9498263f4b7b54465fc
Mariotaku Lee <mariotaku.lee@gmail.com>
2020-12-18 14:48:10 +0900

    Added builder script

--- webos/build-bin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-INSTALL_PREFIX=$PWD/out
-
-cd ../
-./autogen.sh --host=arm-webos-linux-gnueabi --without-libsamplerate --prefix=$INSTALL_PREFIX --with-bashcompletiondir=/dev/null
-make install
